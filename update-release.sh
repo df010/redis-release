cdc236f80eaee4064e3524000df66c571e4c98b7
Dongfei Wu <dwu@Dongfei-Wus-MacBook-Pro.local>
2017-07-12 14:33:04 +0800

    hi

--- update-release.sh
+++ update-release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf ./dev/releases/*
-bosh2 create-release --tarball=./dev_releases/redis-`date +%s`.tar.gz
-bosh2 -e e upload-release ./dev_releases/redis-`date +%s`.tar.gz
+bosh2 create-release --tarball=./dev_releases/redis-release.tar.gz
+bosh2 -e e upload-release ./dev_releases/redis-release.tar.gz
