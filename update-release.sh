526e31cb683581ef0b41df0f1f1b095f8f97ef8a
Dongfei Wu <dwu@Dongfei-Wus-MacBook-Pro.local>
2017-07-12 14:38:39 +0800

    hi

--- update-release.sh
+++ update-release.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
-rm -rf ./dev/releases/*
-bosh2 create-release --tarball=./dev_releases/redis-release.tar.gz
+#!/bin/bash -e
+rm -rf ./dev/releases/*.gz
+newVersion=$((`cat dev_releases/version`+1))
+echo $newVersion > dev_releases/version
+v=0+dev.$newVersion
+
+bosh2 create-release --version=$v --tarball=./dev_releases/redis-release.tar.gz
 bosh2 -e e upload-release ./dev_releases/redis-release.tar.gz
